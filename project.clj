8981fc19ad046c3441e0282d8fff0d150434e63f
Jonathan Chauncey <pair+jchauncey@rallydev.com>
2013-05-08 14:19:46 -0600

    Adding clojars plugin to project.clj, published version 0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject clj-flowdock "0.1.0-SNAPSHOT"
+(defproject com.rallydev/clj-flowdock "0.1.0"
   :description "Clojure library for interacting with flowdock"
   :url "https://github.com/RallySoftware/clj-flowdock"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [clj-http "0.6.3"]
-                 [org.clojure/tools.logging "0.2.6"]])
+                 [org.clojure/tools.logging "0.2.6"]]
+  :plugins [[lein-clojars "0.9.1"]])
