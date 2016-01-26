12c3b0c0635d4a656d3b63cc907cf19f182619f3
Robert Stuttaford <robert@cognician.com>
2016-01-26 08:15:10 +0200

    Update deps.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject com.rallydev/clj-flowdock "1.2.2"
+(defproject com.rallydev/clj-flowdock "1.2.3"
   :description "Clojure library for interacting with flowdock"
   :url "https://github.com/RallySoftware/clj-flowdock"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [clj-http "0.6.3"]
-                 [org.clojure/tools.logging "0.2.6"]]
+                 [clj-http "2.0.1"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [cheshire "5.5.0"]]
   :plugins [[lein-clojars "0.9.1"]])
